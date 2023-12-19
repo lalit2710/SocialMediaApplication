f11709e8920035f6ffddc0accd82b3496b54b2ce
lalit2710 <ltk143.lk@gmail.com>
2023-12-19 12:47:36 +0530

    Final Commit

--- src/main/java/APIwiz/SocialMedia/SecurityConfig.java
+++ src/main/java/APIwiz/SocialMedia/SecurityConfig.java
@@ -3,16 +3,26 @@ package APIwiz.SocialMedia;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
+    public PasswordEncoder passwordEncoder () {
         return new BCryptPasswordEncoder();
+    }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/public/**").permitAll() // Allow access to certain paths
+                .anyRequest().authenticated() // Authenticate for all other paths
+                .and()
+                .httpBasic(); // Use basic authentication
     }
 }
